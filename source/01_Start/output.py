4526dd26efb1620467dfe182a9e37048bd519521
jian1130 <jian11300@gmail.com>
2021-12-23 23:53:58 +0900

    Create output.py

---
+++ source/01_Start/output.py
@@ -0,0 +1,14 @@
+# 한 번만 출력
+print("# 한 번만 출력")
+print("Hello Python Programing...!")
+print()
+
+# 여러 개를 출력
+print("# 여러 개를 출력")
+print(10, 20, 30, 40, 50)
+print("Hello", "Python!")
+print()
+
+# 아무것도 입력하지 않으면 단순하게 줄바꿈
+print("# 아무것도 출력하지 않습니다.")
+print("-- 확인 전용선 --")
