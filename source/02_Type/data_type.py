5e6fa4fb1b07bcf5285b635d35e30613c3d506fa
jian1130 <jian11300@gmail.com>
2021-12-23 23:54:51 +0900

    Create data_type.py

---
+++ source/02_Type/data_type.py
@@ -0,0 +1,9 @@
+# string : "안녕하세요", "Hello World"
+# number : 52, 273, 103.32
+# boolean : True, False
+
+# type : data type check
+
+print(type("안녕하세요"))   # str(string)
+print(type(273))           # int(integer)
+print(type(52.273))           # float(floating point)
