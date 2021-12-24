8837e4d5cc9a2d8d6667fd18cbe52eb53d4c6b0c
jian1130 <jian11300@gmail.com>
2021-12-24 23:02:43 +0900

    Create number_01.py

---
+++ source/03_Number/number_01.py
@@ -0,0 +1,10 @@
+# integer        (0, 1, 273, -52)
+# floating point (0.0, 52.273, -1.2)
+
+print(273)
+print(52.273)
+
+print("0 = ", type(0))
+print("0.0 = ", type(0.0))
+print("52 = ", type(52))
+print("52.273 = ", type(52.273))
