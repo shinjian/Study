061f1173af8c0dfe54ef5eb6380daa0dd1194812
jian1130 <jian11300@gmail.com>
2021-12-24 23:22:34 +0900

    Create operator.py

---
+++ source/03_Number/operator.py
@@ -0,0 +1,12 @@
+# + : Addition(number + number)
+# - : Subtraction(number - number)
+# * : Multiplication(number * number)
+# / : Division(number / number)
+# // : 정수 나누기 연산자(number // number)
+# % : Share(number % number)
+# ** : Square(number ** number)
+
+# string = "string"
+# number = 273
+# string + number
+# => TypeError(string, integer)
