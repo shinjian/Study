418be20eb234acf6ecb7d8fef585bac9ee578216
jian1130 <jian11300@gmail.com>
2021-12-26 20:14:22 +0900

    Create int_convert.py

---
+++ Python Study/source/05_Input/int_convert.py
@@ -0,0 +1,8 @@
+string_a = input("Input A > ")
+int_a = int(string_a)
+
+string_b = input("Input B > ")
+int_b = int(string_b)
+
+print("문자열 자료 : ", string_a + string_b)
+print("숫자 자료 : ", int_a + int_b)
