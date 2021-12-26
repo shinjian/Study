57b8b9815acba1b1b1ecf6f9fd901793540c0338
jian1130 <jian11300@gmail.com>
2021-12-26 20:11:24 +0900

    Create variable03.py

---
+++ Python Study/source/04_Variable/variable03.py
@@ -0,0 +1,20 @@
+# += 숫자 덧셉 후 대입, 문자열 연결 후 대입
+# -= 숫자 뺄셈 후 대입
+# *= 숫자 곱셈 후 대입, 문자열 반복 후 대입
+# /= 숫자 나눗셈 후 대입
+# %= 숫자의 나머지를 구한 후 대입
+# **= 숫자의 제곱 후 대입
+
+number = 100
+number += 10
+number += 20
+number += 30
+print("number : ", number)
+
+string = "Hello"
+string += "!"
+string += "!"
+print("string : ", string)
+
+string *= 3
+print("string : ", string)
