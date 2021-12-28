50aafe80a41433010bb701eb26285640d052ae87
jian1130 <jian11300@gmail.com>
2021-12-28 19:53:22 +0900

    Create format_error01.py

---
+++ Python Study/source/06_Format/format_error01.py
@@ -0,0 +1,10 @@
+format_a = "{} {}".format(1, 2, 3, 4, 5)
+print(format_a)
+
+# IndexError
+# format_b = "{} {} {}".format(1, 2)
+
+# Exception has occurred: IndexError
+# Replacement index 2 out of range for positional args tuple
+#   File "format_error01.py", line 5, in <module>
+#     format_b = "{} {} {}".format(1, 2)
