b02e24c722c70941e7ddd5e8c49e8f9f92ec72c4
jian1130 <jian11300@gmail.com>
2022-01-01 23:00:01 +0900

    Create strip01.py

---
+++ Python Study/source/07_Function/strip01.py
@@ -0,0 +1,14 @@
+# strip() : 문자열 양옆의 공백을 제거
+# lstrip() : 문자열 왼쪽의 공백을 제거
+# rstrip() : 문자열 오른쪽의 공백을 제거
+
+input_a = "    PYTHON    "
+
+print("# 기본 출력")
+print(input_a)
+print("\n# strip() 함수 출력")
+print(input_a.strip())
+print("\n# lstrip() 함수 출력")
+print(input_a.lstrip())
+print("\n# rstrip() 함수 출력")
+print(input_a.rstrip())
