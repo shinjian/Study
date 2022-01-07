61be2f468f07a2152f491e620b15292ed03bb0bf
jian1130 <jian11300@gmail.com>
2022-01-08 03:29:06 +0900

    Create boolean02.py

---
+++ Python Study/source/08_Conditional_Statements/boolean02.py
@@ -0,0 +1,22 @@
+# and operator (그리고)
+# True True = True
+# True False = False
+# False True = False
+# False False = False
+# 그리고
+
+# or operator (또는)
+# True True = True
+# True False = True
+# False True = True
+# False False = False
+
+print(True and True)    # True
+print(True and False)   # False
+print(False and True)   # False
+print(False and False)  # False
+
+print(True or True)     # True
+print(True or False)    # True
+print(False or True)    # True
+print(False or False)   # False
