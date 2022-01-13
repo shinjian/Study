fd215d94cae6f83435696273169fd4c3fe0afece
jian1130 <jian11300@gmail.com>
2022-01-13 10:14:17 +0900

    Create pass_keyword02.py

---
+++ Python Study/source/08_Conditional_Statements/pass_keyword02.py
@@ -0,0 +1,18 @@
+# 입력을 받음
+number = int(input("정수 입력 >> "))
+
+# 조건문 사용 (pass)
+if number > 0:
+    # 양수일 때 : 아직 미구현 상태
+    pass
+else:
+    # 음수일 때 : 아직 미구현 상태
+    pass
+
+# raise NotlmplementedError
+if number > 0:
+    # 양수일 때 : 아직 미구현 상태
+    raise NotImplementedError
+else:
+    # 음수일 때 : 아직 미구현 상태
+    raise NotImplementedError
