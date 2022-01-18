5b184f2184dc9d7d66d27f5042da58dbba1c4083
jian1130 <jian11300@gmail.com>
2022-01-18 19:21:02 +0900

    Create list05.py

---
+++ Python Study/source/09_List_Array/list05.py
@@ -0,0 +1,12 @@
+# list_name.remove(value)
+# list_name.clear()
+
+# 값으로 제거
+list_a = [1, 2, 1, 2]
+list_a.remove(2)
+print("list_a.remove(2) : ", list_a)
+
+# 리스트 내부의 요소를 모두 제거
+list_b = [0, 1, 2, 3, 4, 5]
+list_b.clear()
+print("list_b.clear() : ", list_b)
