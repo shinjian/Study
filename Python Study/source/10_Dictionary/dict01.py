392ea16e57fab90bbbf140e1c718041523f54e90
jian1130 <jian11300@gmail.com>
2022-01-24 13:33:15 +0900

    Create dict01.py

---
+++ Python Study/source/10_Dictionary/dict01.py
@@ -0,0 +1,14 @@
+# variable = {key: value}
+dict_a = {
+    "name": "Avengers Endgame",
+    "type": "Hero Movie"
+}
+print(dict_a)
+print(dict_a["name"])
+print(dict_a["type"])
+
+dict_b = {
+    'director': ['안소니 루소', '조 루소'],
+    'cast': ['아이언맨', '타노스', '토르', '닥터스트레인지', '헐크']
+}
+print(dict_b["director"])
