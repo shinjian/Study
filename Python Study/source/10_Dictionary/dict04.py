c91c053ccbc5e92560c7286dd11e4b1c86c36f2f
jian1130 <jian11300@gmail.com>
2022-01-25 22:08:13 +0900

    Create dict04.py

---
+++ Python Study/source/10_Dictionary/dict04.py
@@ -0,0 +1,13 @@
+# 딕셔너리를 선언
+dictionary = {}
+
+# 요소 추가 전에 내용을 출력
+print("요소 추가 이전: ", dictionary)
+
+# 딕셔너리에 요소를 추가
+dictionary["name"] = "새로운 이름"
+dictionary["head"] = "새로운 정신"
+dictionary["body"] = "새로운 몸"
+
+# 출력
+print("요소 추가 이후: ", dictionary)
