1966c8399a701811ffc0a5e44760a5199bf84189
jian1130 <jian11300@gmail.com>
2022-01-26 18:54:56 +0900

    Create dict08_for.py

---
+++ Python Study/source/10_Dictionary/dict08_for.py
@@ -0,0 +1,15 @@
+# for key variable in dictionary:
+#   Code
+
+# 딕셔너리를 선언
+dictionary = {
+    "name": "7D 건조 망고",
+    "type": "당절임",
+    "ingredient": ["망고", "설탕", "메타중아황산나트륨", "치자황색소"],
+    "origin": "필리핀"
+}
+
+# for 반복문을 사용
+for key in dictionary:
+    # 출력
+    print(key, ':', dictionary[key])
