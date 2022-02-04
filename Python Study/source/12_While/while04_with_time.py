aed31dd55ae7f96e7c227f8453ffa1ae308b92a4
jian1130 <jian11300@gmail.com>
2022-02-04 23:57:55 +0900

    Create while04_with_time.py

---
+++ Python Study/source/12_While/while04_with_time.py
@@ -0,0 +1,14 @@
+# 시간과 관련된 기능을 가져옴
+from ctypes.wintypes import tagRECT
+import time
+
+# 변수를 선언
+number = 0
+
+# 5초 동안 반복
+target_tick = time.time() + 5
+while time.time() < target_tick:
+    number += 1
+
+# 출력
+print("5초 동안 {}번 반복했습니다.".format(number))
